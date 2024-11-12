dc61f08be894f59de9cc1852bc5790924d4257e8
Ouchin <ouchinmustapha82@gmail.com>
2024-11-12 11:39:43 +0100

    Fist commit

---
+++ src/main/java/com/ouchin/WRM/WrmApplication.java
@@ -0,0 +1,13 @@
+package com.ouchin.WRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ouchin/WRM/WrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouchin.WRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
