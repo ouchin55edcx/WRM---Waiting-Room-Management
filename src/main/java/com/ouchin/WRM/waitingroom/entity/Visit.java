6a135dc0417aa6a02c0ad2199a7f3d1d4cc5b2a4
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-12 19:56:02 +0100

    add embedded id for visit

--- src/main/java/com/ouchin/WRM/waitingroom/entity/Visit.java
+++ src/main/java/com/ouchin/WRM/waitingroom/entity/Visit.java
@@ -4,6 +4,7 @@ import com.ouchin.WRM.visitor.entity.Visitor;
 import com.ouchin.WRM.waitingroom.entity.enums.Status;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import com.ouchin.  ;;
 
 import java.time.Duration;
 import java.time.LocalTime;
@@ -11,9 +12,8 @@ import java.time.LocalTime;
 @Entity
 public class Visit {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Embedded
+    private VisitId id;
 
     @NotNull(message = "Arrival time is required")
     private LocalTime arrivalTime;
